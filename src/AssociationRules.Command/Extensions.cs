b0ed78b61001b69633f054ab562df2fb80e1df18
Dan Swain <dan.swain@gmail.com>
2011-09-19 17:24:15 +0100

    initial commit to data mining

---
+++ src/AssociationRules.Command/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataMining.MarketBasketAnalysis
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Combine<T>(this IEnumerable<T> collection,IEnumerable<T> anotherCollection)
+        {
+            var combinedCollection = new List<T>();
+            combinedCollection.AddRange(collection.Select(x => x));
+            combinedCollection.AddRange(anotherCollection.Select(x => x));
+
+            return combinedCollection;
+        }
+
+        public static IEnumerable<T>  CombineSets<T>(this IEnumerable<T> collection)
+        {
+            var combined = new List<T>();
+            foreach (var item in collection)
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/AssociationRules.Command/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataMining.MarketBasketAnalysis
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var set1 = new int[] { 1, 2, 3, 4 };
+            var set2 = new int[] { 1, 2 };
+            var set3 = new int[] { 1, 3, 4 };
+            var set4 = new int[] { 2, 3 };
+            var set5 = new int[] { 3, 4 };
+            var set6 = new int[] { 2, 4 };
+
+            List<int[]> setsCollection = new List<int[]>{set1,set2,set3,set4,set5,set6};
+
+            var combinedSets = set1
+                .Combine(set2)
+                .Combine(set3)
+                .Combine(set4)
+                .Combine(set5)
+                .Combine(set6);
+
+
+
+            var frequencies = GetFrequencies(combinedSets);
+
+            var minSupportReached = frequencies.Where(x => x.Frequency >= 2);
+
+
+            var supports = GenerateCandidates(minSupportReached.ToList(), (name, firstVal, secondVal) =>
+                                                                             {
+                                                                                 int count = 0;
+
+                                                                                 foreach (var set in setsCollection)
+                                                                                 {
+                                                                                     if (set.Contains(firstVal) && set.Contains(secondVal))
+                                                                                         count++;
+                                                                                 }
+                                                                                 return count;
+
+
+
+                                                                             });
+
+            var minSets = supports.Where(x => x.Value >= 2).ToDictionary(x => x.Key);
+
+
+
+            Console.ReadLine();
+        }
+
+        private static Dictionary<string, int> GenerateCandidates(IList<Item> minSupportReached, Func<string, int, int, int> func)
+        {
+            var candidateSupport = new Dictionary<string, int>();
+
+            for (int i = 0; i < minSupportReached.Count; i++)
+            {
+                for (int j = i + 1; j < minSupportReached.Count; j++)
+                {
+                    var candidateName = minSupportReached[i].Name + "-" + minSupportReached[j].Name;
+                    int support = func(candidateName, minSupportReached[i].Value, minSupportReached[j].Value);
+                    candidateSupport.Add(candidateName, support);
+                }
+            }
+
+            return candidateSupport;
+        }
+
+        private static IEnumerable<Item> GetFrequencies(IEnumerable<int> dataSet)
+        {
+
+
+
+            var result = dataSet.Distinct().Select(x => new Item
+                                                        {
+                                                            Value = x,
+                                                            Name = x.ToString(),
+                                                            Frequency = dataSet.Count(y => y == x)
+                                                        });
+
+
+            return result;
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Frequency { get; set; }
+
+        public int Value { get; set; }
+    }
+}
